cb9858c785a5b4bb708d4d09ea88bc4c5b44405a
pawanbondre67 <bondrepavan623@gmail.com>
2024-08-24 23:46:18 +0530

    core module added

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,23 +2,30 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './auth/login/login.component';
 import { FormsModule } from '@angular/forms';
-import { LoaderComponent } from '../utility/loader/loader.component';
+import { SharedModule } from '../utility/shared.module';
+import { AuthService } from './auth/services/auth.service';
+import { CoreRoutingModule } from './core-routing.module';
+
 
 
 
 
 @NgModule({
   declarations: [
-    LoginComponent,
-    LoaderComponent
+    LoginComponent
   ],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    SharedModule,
+    CoreRoutingModule
   ],
   exports: [
-    LoginComponent,
-    LoaderComponent
+    LoginComponent
+  ],
+
+  providers: [
+     AuthService
   ]
 })
 export class CoreModule { }
